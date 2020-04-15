b0845ab758af2d7ef24aebea8d62d538be5852ae
liangdong <1825044843@qq.com>
2020-04-15 16:02:16 +0800

    编辑添加

--- src/views/room/index.vue
+++ src/views/room/index.vue
@@ -15,7 +15,10 @@
       @row-add="handleRowAdd"
       @dialog-cancel="handleDialogCancel"
       :rowHandle="rowHandle"
-      @row-remove="handleRowRemove">
+      @row-remove="handleRowRemove"
+      edit-title="我的修改"
+      :edit-template="editTemplate"
+      @row-edit="handleRowEdit">
       <el-button slot="header" style="margin-bottom: 5px" @click="addRow"><i class="fa fa-plus" aria-hidden="true"></i> 新增</el-button>
       <el-input slot="header" style="margin-bottom: 5px" placeholder="会议室编号" suffix-icon="el-icon-search"> </el-input>
       <el-button slot="header" style="margin-bottom: 5px"><i class="el-icon-search"></i> 搜索</el-button>
@@ -25,8 +28,7 @@
 </template>
 
 <script>
-// eslint-disable-next-line no-unused-vars
-import { RoomList } from '@/api/room'
+// import { BusinessTable1List } from '@api/demo.business.table.1'
 export default {
   data () {
     return {
@@ -68,6 +70,47 @@ export default {
           key: 'state'
         }
       ],
+      data: [
+        {
+          roomid: '1',
+          address: '1-101',
+          table: '100',
+          chair: '100',
+          project: '1',
+          microphone: '1',
+          computer: '1',
+          number: '100',
+          state: '30',
+          forbidRemove: false,
+          showRemoveButton: true
+        },
+        {
+          roomid: '1',
+          address: '1-101',
+          table: '100',
+          chair: '100',
+          project: '1',
+          microphone: '1',
+          computer: '1',
+          number: '100',
+          state: '30',
+          forbidRemove: false,
+          showRemoveButton: true
+        },
+        {
+          roomid: '1',
+          address: '1-101',
+          table: '100',
+          chair: '100',
+          project: '1',
+          microphone: '1',
+          computer: '1',
+          number: '100',
+          state: '30',
+          forbidRemove: false,
+          showRemoveButton: true
+        }
+      ],
       addTemplate: {
         address: {
           title: '地点',
@@ -102,6 +145,54 @@ export default {
           value: '20'
         }
       },
+      editTemplate: {
+        address: {
+          title: '地点',
+          value: ''
+        },
+        table: {
+          title: '桌子',
+          value: ''
+        },
+        chair: {
+          title: '椅子',
+          value: ''
+        },
+        project: {
+          title: '投影仪',
+          value: ''
+        },
+        microphone: {
+          title: '麦克风',
+          value: ''
+        },
+        computer: {
+          title: '电脑',
+          value: ''
+        },
+        number: {
+          title: '人数',
+          value: ''
+        },
+        state: {
+          title: '状态',
+          value: ''
+        },
+        forbidEdit: {
+          title: '禁用按钮',
+          value: false,
+          component: {
+            show: false
+          }
+        },
+        showEditButton: {
+          title: '显示按钮',
+          value: true,
+          component: {
+            show: false
+          }
+        }
+      },
       formOptions: {
         labelWidth: '80px',
         labelPosition: 'left',
@@ -115,7 +206,6 @@ export default {
       },
       rowHandle: {
         remove: {
-          icon: 'el-icon-delete',
           size: 'small',
           fixed: 'right',
           confirm: true,
@@ -130,6 +220,26 @@ export default {
               return true
             }
             return false
+          }
+        },
+        edit: {
+          columnHeader: '编辑表格',
+          edit: {
+            icon: 'el-icon-edit',
+            text: '点我进行编辑',
+            size: 'small',
+            show (index, row) {
+              if (row.showEditButton) {
+                return true
+              }
+              return false
+            },
+            disabled (index, row) {
+              if (row.forbidEdit) {
+                return true
+              }
+              return false
+            }
           }
         }
       }
@@ -159,7 +269,6 @@ export default {
           message: '保存成功',
           type: 'success'
         })
-
         // done可以传入一个对象来修改提交的某个字段
         done({
           roomid: '2',
@@ -192,6 +301,24 @@ export default {
           done()
         }, 300)
       }
+    },
+
+    handleRowEdit ({ index, row }, done) {
+      this.formOptions.saveLoading = true
+      setTimeout(() => {
+        console.log(index)
+        console.log(row)
+        this.$message({
+          message: '编辑成功',
+          type: 'success'
+        })
+
+        // done可以传入一个对象来修改提交的某个字段
+        done({
+          address: '我是通过done事件传入的数据！'
+        })
+        this.formOptions.saveLoading = false
+      }, 300)
     }
   }
 }
